b6379ddc24b89f67dcd54ea3a0af5544bcbe90aa
letyresina <leticiaresina@gmail.com>
2021-06-21 21:18:27 -0300

    Adição da funcionalidade dos botões de navegação

--- app/src/main/java/com/example/projlolpam/ItensActivity.java
+++ app/src/main/java/com/example/projlolpam/ItensActivity.java
@@ -2,7 +2,9 @@ package com.example.projlolpam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ItensActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class ItensActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itens);
+    }
+
+    public void abrirHomeActivity(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void abrirCampeoesActivity(View view) {
+        Intent intent = new Intent(this, CampeoesActivity.class);
+        startActivity(intent);
     }
 }
