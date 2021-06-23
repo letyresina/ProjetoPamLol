3166026239534f3de72d7413a9097badce4ad2c9
letyresina <leticiaresina@gmail.com>
2021-06-23 18:11:37 -0300

    Adição da campeã Katarina

---
+++ app/src/main/java/com/example/projlolpam/KatarinaActivity.java
@@ -0,0 +1,14 @@
+package com.example.projlolpam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KatarinaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_katarina);
+    }
+}
