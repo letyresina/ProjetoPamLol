ec4c4b1fb7078f2fc5d98878cd0a7bde7ac3c34c
TaveiraTavel <mateus.staveira@gmail.com>
2021-06-23 00:05:56 -0300

    Conclusão do design da tela do campeão e adição da campeã Kalista.

--- app/src/main/java/com/example/projlolpam/KalistaActivity.java
+++ app/src/main/java/com/example/projlolpam/KalistaActivity.java
@@ -2,7 +2,9 @@ package com.example.projlolpam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class KalistaActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class KalistaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalista);
+    }
+
+    public void abrirHomeActivity(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void abrirItensActivity(View view) {
+        Intent intent = new Intent(this, ItensActivity.class);
+        startActivity(intent);
     }
 }
